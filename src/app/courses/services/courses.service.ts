7ded80e5ac33171f4a836f763e5ca36ab19dc611
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-09-11 06:51:57 -0300

    Http Get

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  findAll(): Course[] {
-    return [
-      {_id: '1', name: 'Angular', category: 'front-end'},
-      {_id: '2', name: 'Java', category: 'back_end'}
-    ];
+  findAll() {
+    return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+        tap(course => console.log(course))
+      );
   }
 }
