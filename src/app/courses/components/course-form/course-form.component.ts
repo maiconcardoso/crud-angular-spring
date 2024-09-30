d36cda3bfc9d21bd4f428ca7d9d4d86f810539a7
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-09-30 12:41:05 -0300

    Validators form

--- src/app/courses/components/course-form/course-form.component.ts
+++ src/app/courses/components/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../../services/courses.service';
 import { ActivatedRoute } from '@angular/router';
@@ -15,8 +15,8 @@ export class CourseFormComponent {
 
   form = this.formBuilder.group({
       _id: [''],
-      name: [''],
-      category: ['']
+      name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(80)]],
+      category: ['', [Validators.required]]
   });
 
   constructor(private formBuilder: NonNullableFormBuilder,
@@ -50,5 +50,25 @@ export class CourseFormComponent {
   onSuccess() {
     this.snackBar.open('Curso criado com sucesso!', '', {duration:3000});
     this.onCancel();
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo Obrigatório'
+    }
+
+    if (field?.hasError('minLength')) {
+      const requiredLength: number = field.errors ? field.errors['minLength']['requiredLength'] : 5
+      return `Tamanho minímo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxLength')) {
+      const requiredLength: number = field.errors ? field.errors['maxLength']['requiredLength'] : 80
+      return `Tamanho maxímo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo inválido.'
   }
 }
